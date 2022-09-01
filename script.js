8a37de690e1d7d0e366cd782a06f06fe06aff7ad
Glitch (glitch-hello-website) <none>
2022-09-01 17:18:07 +0000

    🏓🌉 Checkpoint ./script.js:42900803/446

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-gsap.from("#header", {y:-300, delay:0.2, duration:3, opacity:0.5, ease:"power4.out"})
-gsap.from("#header", {y:-300, delay:0.2, duration:3, opacity:0.5, ease:"power4.out"})
-gsap.from("#header", {y:-300, delay:0.2, duration:3, opacity:0.5, ease:"power4.out"})
+gsap.from(".container", {y:-300, delay:0.2, duration:3, opacity:0, ease:"power4.out"})
+gsap.from("h1", {x:-300, delay:0.4, duration:3, opacity:0, ease:"power4.out"})
+gsap.from("h3", {x:-300, delay:1.5, duration:3, opacity:0, ease:"power4.out"})
+gsap.from("span", {x:300, delay:2.5, duration:3, opacity:0, ease:"power4.out"})
 
 
 const btn= document.querySelector("#btn");
@@ -21,7 +22,7 @@ e.preventDefault();
     let fun= document.querySelector("#fun").value;
 
 
-if ( people<=0  || people ===" "|| plane<0 || train <0 || bus<0 || taxi<0 || hotel<0 || food<0|| car<0 ||fun<0 || || isNaN(people) || isNaN(plane) || isNaN(train) || isNaN(bus) || isNaN(taxi) || isNaN(hotel) || isNaN(food) || isNaN(car) || isNaN(fun){
+if (people<=0  || people ===" "|| plane<0 || train <0 || bus<0 || taxi<0 || hotel<0 || food<0|| car<0 ||fun<0 || isNaN(people) || isNaN(plane) || isNaN(train) || isNaN(bus) || isNaN(taxi) || isNaN(hotel) || isNaN(food) || isNaN(car) || isNaN(fun))  {
     Swal.fire({
         icon: 'error',
         title: 'Faltan datos',
