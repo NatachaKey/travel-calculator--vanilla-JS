cf56b129c1ae5dc177d1ce672a45870a9a8d4158
Glitch (glitch-hello-website) <none>
2022-10-25 17:13:35 +0000

    💆🌝 Checkpoint ./index.html:42900803/4341 ./script.js:42900803/9 ./style.css:42900803/2262

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-gsap.from(".container", {y:100, delay:0.2, duration:3, opacity:0, ease:"power4.out"})
-gsap.from("h1", {x:-300, delay:0.4, duration:3, opacity:0, ease:"power4.out"})
-gsap.from("h3", {x:-300, delay:1.5, duration:3, opacity:0, ease:"power4.out"})
-gsap.from("span", {x:300, delay:2.5, duration:3, opacity:0, ease:"power4.out"})
-gsap.from("#btn", {y:100, delay:3, duration:3, opacity:0, ease:"power4.out"})
-gsap.from(".block", {y:100, delay:3.2, duration:3, opacity:0, ease:"power4.out"})
+gsap.from(".container", {y:100, delay:0.2, duration:3, opacity:0, ease:"power4.out"});
+gsap.from("h1", {x:-300, delay:0.4, duration:3, opacity:0, ease:"power4.out"});
+gsap.from("h3", {x:-300, delay:1.5, duration:3, opacity:0, ease:"power4.out"});
+gsap.from("span", {x:300, delay:2.5, duration:3, opacity:0, ease:"power4.out"});
+gsap.from("#btn", {y:100, delay:3, duration:3, opacity:0, ease:"power4.out"});
+gsap.from(".block", {y:100, delay:3.2, duration:3, opacity:0, ease:"power4.out"});
 
 const btn= document.querySelector("#btn");
 
@@ -33,7 +33,6 @@ if (people<=0  || people ===" "|| plane<0 || train <0 || bus<0 || taxi<0 || hote
 return false;
 }
 
-
 let totalSum= + plane + + train + + bus + + taxi + + hotel + + food + + car + + fun;
 totalSum=totalSum.toFixed(2);
 
@@ -43,6 +42,4 @@ let result= totalSum / people;
 result= result.toFixed(2);
 
 document.querySelector("#totalPerPerson").textContent= result;
-
-
 }
